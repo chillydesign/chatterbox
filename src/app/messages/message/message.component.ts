f56ea29a2c0b62bfba9bff7aa0ee1b7c6623cd0b
Charles Harvey <harvey.charles@gmail.com>
2020-03-31 21:34:00 +0200

    add lazy load images

--- src/app/messages/message/message.component.ts
+++ src/app/messages/message/message.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Input, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, Output, EventEmitter, HostListener, ElementRef, ViewChild } from '@angular/core';
 import { Message } from 'src/app/models/message.model';
 import { User } from 'src/app/models/user.model';
 import { MessagesService } from 'src/app/services/messages.service';
 import { Subscription } from 'rxjs';
+import { WinrefService } from 'src/app/services/winref.service';
 
 @Component({
   selector: 'app-message',
@@ -13,12 +14,16 @@ export class MessageComponent implements OnInit, OnDestroy {
   @Input() message: Message;
   @Input() current_user: User;
   @Output() messageDeleted: EventEmitter<Message | null | undefined> = new EventEmitter(undefined);
-
+  @ViewChild('messageContainer', { read: ElementRef }) messageContainer: ElementRef;
+  public file_url: string;
+  public scrollY = 0;
+  public inViewport = false;
   private delete_sub: Subscription;
-  constructor(private messagesService: MessagesService) { }
+  constructor(private messagesService: MessagesService, private winref: WinrefService) { }
 
   ngOnInit() {
-
+    // if in viewport load any picture necessary, this will also be called on scroll
+    setTimeout(() => this.checkIsPostInViewport(), 1000);
   }
 
   deleteMessage(): void {
@@ -32,6 +37,43 @@ export class MessageComponent implements OnInit, OnDestroy {
       }
     }
   }
+
+
+
+  checkIsPostInViewport(): void {
+    if (this.inViewport === false) {
+      // console.log('checkIsPostInViewport', this.spacepost.id);
+      if (this.message) {
+
+        const winHeight = (this.winref.nativeWindow.innerHeight);
+        const nat = this.messageContainer.nativeElement;
+        const natHeight = nat.clientHeight;
+        const natOffset = nat.offsetTop;
+        if ((this.scrollY + winHeight) > (natOffset - natHeight)) {
+          this.inViewport = true;
+          this.lazyloadImages();
+        }
+      }
+    }
+
+  }
+
+
+  lazyloadImages(): void {
+    if (this.message.file_url) {
+      this.file_url = this.message.file_url;
+    }
+  }
+
+
+  // on scroll check if in viewpoirt, if so can load video
+  @HostListener('window:scroll', ['$event']) onKeydownHandler(event) {
+    this.scrollY = (this.winref.nativeWindow.scrollY);
+    this.checkIsPostInViewport();
+  }
+
+
+
 
   ngOnDestroy() {
     const subs: Subscription[] = [
