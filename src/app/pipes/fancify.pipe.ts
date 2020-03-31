79b61916465a1beec97656cfcca072cbe5363fac
Charles Harvey <harvey.charles@gmail.com>
2020-03-31 13:43:14 +0200

    various style changes

--- src/app/pipes/fancify.pipe.ts
+++ src/app/pipes/fancify.pipe.ts
@@ -10,23 +10,23 @@ export class FancifyPipe implements PipeTransform {
 
   transform(text: string): string {
 
-    // const asterisks = /\*\*(\S([^*]*?\S)?)\*\*/g;
-    const asterisks = /\*(\S([^*]*?\S)?)\*/g;
-    while (text.match(asterisks)) {
-      const asterisk_matches = text.match(asterisks);
-      asterisk_matches.forEach((match) => {
-        const newText = match.split('*');
-        text = text.replace(match, `<strong style="font-weight:bold">${newText[1]}</strong>`);
-      });
-    }
-    const underscores = /\_(\S([^_]*?\S)?)\_/g;
-    while (text.match(underscores)) {
-      const underscore_matches = text.match(underscores);
-      underscore_matches.forEach((match) => {
-        const newText = match.split('_');
-        text = text.replace(match, `<em style="font-style:italic">${newText[1]}</em>`);
-      });
-    }
+
+    // const asterisks = /\*(\S([^*]*?\S)?)\*/g;
+    // while (text.match(asterisks)) {
+    //   const asterisk_matches = text.match(asterisks);
+    //   asterisk_matches.forEach((match) => {
+    //     const newText = match.split('*');
+    //     text = text.replace(match, `<strong style="font-weight:bold">${newText[1]}</strong>`);
+    //   });
+    // }
+    // const underscores = /\_(\S([^_]*?\S)?)\_/g;
+    // while (text.match(underscores)) {
+    //   const underscore_matches = text.match(underscores);
+    //   underscore_matches.forEach((match) => {
+    //     const newText = match.split('_');
+    //     text = text.replace(match, `<em style="font-style:italic">${newText[1]}</em>`);
+    //   });
+    // }
 
 
 
