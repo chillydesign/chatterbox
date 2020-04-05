27d5c01eec99fb2cef73ac2a654616eda1b8e451
Charles Harvey <harvey.charles@gmail.com>
2020-04-05 15:56:59 +0200

    change prod front url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   site_name: `Chatterbox`,
-  api_url: 'https://webfactor.ch/chatterbox_api',
-  front_url: 'https://webfactor.ch/chatterbox',
-  cookie_domain: 'webfactor.ch',
+  api_url: 'https://bangorchat.site/chatterbox_api',
+  front_url: 'https://bangorchat.site',
+  cookie_domain: 'bangorchat.site',
   cookie_length_hours: 16,
   cookie_name: 'token_chatterbox',
   cache_duration: 60000, // time to hold resources from API in cache in milliseconds
